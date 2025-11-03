89cd45dd57ef46d469b843005a32ec90fd841916
Aakash Deep Yadav <aakashdeepyadav106@gmail.com>
2025-11-04 03:42:39 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import AddingItemContainer from "./AddingItemContainer";
 import TodoColumns from "./TodoColumns";
 import DisplayTodoItems from "./DisplayTodoItems";
 function App() {
-  let arr = ["Eat food", "Create PPT", "Solve DSA", "Upload project report"];
+  let arr = [
+    { task: "Eat food", due: "10/11/25" },
+    { task: "Create PPT", due: "11/11/25" },
+    { task: "Solve DSA", due: "12/11/25" },
+    { task: "Upload project report", due: "13/11/25" },
+  ];
 
   return (
     <>
--- src/DisplayTodoItems.jsx
+++ src/DisplayTodoItems.jsx
@@ -5,8 +5,8 @@ function DisplayTodoItems({ tasks }) {
         <tbody>
           {tasks.map((item, index) => (
             <tr key={index}>
-              <td>{item}</td>
-              <td>03/11/25</td>
+              <td>{item.task}</td>
+              <td>{item.due}</td>
               <td>
                 <button>Done</button>
               </td>
